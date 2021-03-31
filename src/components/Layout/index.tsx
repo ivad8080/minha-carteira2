1e911ba696a6d3b833fe1b5d91f745e8f6214bdb
Davi SAS <ivad8080@gmail.com>
2021-03-31 17:19:21 -0300

    Minor Updates

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Grid } from './styles';
 
 import MainHeader from '../MainHeader';
 import Aside from '../Aside';
@@ -8,11 +8,11 @@ import Content from '../Content';
 
 const Layout: React.FC = () => {
     return (
-        <Container>
+        <Grid>
             <MainHeader />
             <Aside />
             <Content />
-        </Container>
+        </Grid>
     );
 }
 
--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -1,3 +1,21 @@
  import styled from 'styled-components';
 
- export const Container = styled.div``;
+    /**
+    * Layout
+    * MH - MainHeader
+    * AS - Aside
+    * CT - Content
+    */
+
+export const Grid = styled.div`
+    display: grid;
+    grid-template-columns: 250px auto;
+    grid-template-rows: 70px auto;
+
+    grid-template-areas:
+    'AS MH'
+    'AS CT';
+
+    height: 100vh;
+
+ `;
